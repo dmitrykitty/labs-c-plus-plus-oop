468e4bcbfb8eba872dc347df69fec43c2b7a0ebf
Grzegorz Bazior (AGH) <bazior@agh.edu.pl>
2024-03-18 10:52:01 +0100

    Added first version for year 2024

---
+++ simpleString.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <utility> // std::exchange
+using namespace std;
+
+#include "simpleString.h"
+
+#warning "Funkcje sa do zaimplementowania. Instrukcja w pliku naglowkowym" // po zaimplementowaniu sugeruje usuniecie tej linijki - nie chcemy warningow
+
